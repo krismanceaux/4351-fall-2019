12e13aa4e0cba42271d79ab1448b6fc7a39b1e34
Kristopher Manceaux <krismanceaux@hotmail.com>
2019-11-23 03:43:39 -0600

    Added functionality to insert new role to database

--- client/src/Pages/AdminRoles.js
+++ client/src/Pages/AdminRoles.js
@@ -1,8 +1,10 @@
 import React, { useState, Component } from 'react';
 import AdminCard from '../components/AdminCard';
+import { Grid, TextField, Button } from '@material-ui/core';
 
 class AdminRoles extends Component {
   state = {
+    adminType: '',
     adminList: []
   };
 
@@ -15,10 +17,60 @@ class AdminRoles extends Component {
       .catch(err => console.log(err));
   }
 
+  handleAdminType = text => {
+    this.setState({ adminType: text.target.value });
+  };
+
+  Submit = () => {
+    const adtype = this.state.adminType;
+    console.log('type: ' + adtype);
+    //console.log(this.state.adminType);
+    fetch(`http://localhost:5000/addToAdminList`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        adminType: this.state.adminType
+      })
+    })
+      .then(res => res.json())
+      .catch(err => console.log(err));
+  };
+
   render() {
-    return this.state.adminList.map(admin => (
-      <AdminCard admin={{ ...admin }} />
-    ));
+    return (
+      <React.Fragment>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <TextField
+              name="adminType"
+              variant="outlined"
+              fullWidth
+              id="adminType"
+              label="Admin Type"
+              value={this.state.adminType}
+              onChange={this.handleAdminType}
+            />
+          </Grid>
+
+          <Grid item xs={4}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              style={{ margin: '24px 0px 16px' }}
+              onClick={this.Submit}
+            >
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+        {this.state.adminList.map(admin => (
+          <AdminCard admin={{ ...admin }} />
+        ))}
+      </React.Fragment>
+    );
   }
 }
 
